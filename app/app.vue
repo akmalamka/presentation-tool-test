63ee2ff8e90b44b5d2b5772169c4ebf0c39653cf
akmalamka <muhammad.akmal@incentro.com>
2025-01-17 17:32:06 +0800

    feat: nuxt with sanity boilerplate

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const query = groq`*[_type == "post"]`
+const { data } = useSanityQuery(query)
+watchEffect(() => console.log(data.value))
+</script>
+
+<template>
+  <div>
+    <h1>{{data?.[0].title}}</h1>
+  </div>
+</template>
